43e9ba4114caf3c263a62a4ad8c6ad93fe3d809a
sachin metkari <sachinmetkari89@gmail.com>
2017-03-20 22:13:40 +0530

    added filter and highlight functionality

--- app/helpers/uplaod_zip_files_helper.rb
+++ app/helpers/uplaod_zip_files_helper.rb
@@ -8,14 +8,26 @@ def read_file(file, xml_file_name)
       zipfile.each do |entry|
         @xml_file_names << entry.name if (entry.name =~ /.xml$/).present?
         if xml_file_name == entry.name
+          @content << entry.get_input_stream.read && break if params[:filter].blank?
           parse_xml_string(entry)
         end
       end
     end
+    rescue 
+    flash[:error] = 'Can not read zip file.'  
 end
 
 def parse_xml_string(entry)
-    @content = Nokogiri::XML.parse(entry.get_input_stream.read)
+    @random_string = [*('A'..'Z'),*('a'..'z')].sample(20).join
+    @doc = Nokogiri::XML.parse(entry.get_input_stream.read)
+    results = @doc.xpath(params[:filter])
+    results.map do |x|
+      node = Nokogiri::XML::Node.new @random_string, @doc
+      x.add_previous_sibling(node) if @doc.root != x
+      @content << x.to_s
+    end
+    rescue
+      flash[:error] = 'X-path are not correct.'
 end
 
 end
